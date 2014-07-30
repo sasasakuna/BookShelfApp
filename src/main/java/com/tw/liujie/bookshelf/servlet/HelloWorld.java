594b4241e126e72ad012a8a50259d8aeb6cf6b85
jjliu <ecliujie@gmail.com>
2014-07-30 21:54:44 +0800

    update the jsp and servlet

--- src/main/java/com/tw/liujie/bookshelf/servlet/HelloWorld.java
+++ src/main/java/com/tw/liujie/bookshelf/servlet/HelloWorld.java
@@ -1,16 +1,15 @@
 package com.tw.liujie.bookshelf.servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
